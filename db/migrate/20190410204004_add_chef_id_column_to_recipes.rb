cd1785bb2d62ebdb6d4a59401057b3da9e34c972
Tricia Sykes <tlsykes98@gmail.com>
2019-04-10 14:02:20 -0700

    add one to many associations

---
+++ db/migrate/20190410204004_add_chef_id_column_to_recipes.rb
@@ -0,0 +1,5 @@
+class AddChefIdColumnToRecipes < ActiveRecord::Migration[5.2]
+  def change
+    add_column :recipes, :chef_id, :integer
+  end
+end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: "Stir Fry", description: "veggies in a pot")
+    @chef = Chef.create(chefname: "tricia", email: "tricia@example.com")
+    @recipe = @chef.recipes.build(name: "Stir Fry", description: "veggies in a pot")
+  end
+
+  test "recipe without chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
 
   test "recipe should be valid" do
