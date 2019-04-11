d97db6b91bfceb6e00800bab703ec4bcbca8bdf2
Tricia Sykes <tlsykes98@gmail.com>
2019-04-10 20:49:13 -0700

    added show page

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -16,6 +16,15 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get recipes_path
     assert_template 'recipes/index'
-    assert_match @recipe.name, response.body
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
   end
+
+    test "should get recipe show" do
+      get recipe_path(@recipe)
+      assert_template 'recipes/show'
+      assert_match @recipe.name, response.body
+      assert_match @recipe.description, response.body
+      assert_match @chef.chefname
+
+    end
 end
